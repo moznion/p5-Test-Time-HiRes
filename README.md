7ea7319db03cb77558c97ce42825a17418eb4195
moznion <moznion@gmail.com>
2017-05-09 23:57:34 +0900

    Minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Test::Time::HiRes - It's new $module
+
+# SYNOPSIS
+
+    use Test::Time::HiRes;
+
+# DESCRIPTION
+
+Test::Time::HiRes is ...
+
+# LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+moznion <moznion@gmail.com>
---
+++ lib/Test/Time/HiRes.pm
@@ -0,0 +1,39 @@
+package Test::Time::HiRes;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::Time::HiRes - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test::Time::HiRes;
+
+=head1 DESCRIPTION
+
+Test::Time::HiRes is ...
+
+=head1 LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+moznion E<lt>moznion@gmail.comE<gt>
+
+=cut
+
